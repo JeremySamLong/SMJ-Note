650f1be193c9216cc79dc1b9bc9cbf7937f3d89b
Mael1 <mael.barbier@sfr.fr>
2014-07-03 16:45:28 +0200

    Nouvelle interface

--- gen/com/example/smj_note/R.java
+++ gen/com/example/smj_note/R.java
@@ -10,6 +10,9 @@ package com.example.smj_note;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int bordeaux=0x7f060000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -22,28 +25,35 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int create_enreg=0x7f020000;
+        public static final int create_note=0x7f020001;
+        public static final int create_sync=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int notelist=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080012;
-        public static final int body=0x7f08000b;
-        public static final int button1=0x7f080009;
-        public static final int buttonNote=0x7f080000;
-        public static final int buttonPlayRec=0x7f08000f;
-        public static final int buttonRec=0x7f080001;
-        public static final int buttonStopPlay=0x7f080010;
-        public static final int buttonStopRec=0x7f08000e;
-        public static final int buttonSync=0x7f080002;
-        public static final int checkBox1=0x7f080008;
-        public static final int confirm=0x7f08000c;
-        public static final int editText1=0x7f080006;
-        public static final int editText2=0x7f080007;
-        public static final int listView1=0x7f080011;
-        public static final int text1=0x7f08000d;
-        public static final int textView1=0x7f080003;
-        public static final int textView2=0x7f080004;
-        public static final int textView3=0x7f080005;
-        public static final int title=0x7f08000a;
+        public static final int action_settings=0x7f090015;
+        public static final int addenregbutton=0x7f090003;
+        public static final int addnotebutton=0x7f090002;
+        public static final int addsyncbutton=0x7f090005;
+        public static final int body=0x7f09000e;
+        public static final int button1=0x7f09000b;
+        public static final int buttonNote=0x7f090000;
+        public static final int buttonPlayRec=0x7f090012;
+        public static final int buttonRec=0x7f090001;
+        public static final int buttonStopPlay=0x7f090013;
+        public static final int buttonStopRec=0x7f090011;
+        public static final int buttonSync=0x7f090004;
+        public static final int checkBox1=0x7f09000c;
+        public static final int confirm=0x7f09000f;
+        public static final int editText1=0x7f090009;
+        public static final int editText2=0x7f09000a;
+        public static final int listView1=0x7f090014;
+        public static final int text1=0x7f090010;
+        public static final int textView1=0x7f090006;
+        public static final int textView2=0x7f090008;
+        public static final int textView3=0x7f090007;
+        public static final int title=0x7f09000d;
     }
     public static final class layout {
         public static final int accueil=0x7f030000;
@@ -57,14 +67,14 @@ public final class R {
         public static final int rec_liste=0x7f030008;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int body=0x7f05000a;
-        public static final int confirm=0x7f05000b;
-        public static final int edit_note=0x7f05000c;
+        public static final int body=0x7f05000b;
+        public static final int confirm=0x7f05000c;
+        public static final int edit_note=0x7f05000d;
         public static final int hello_world=0x7f050002;
         public static final int ma_fiche=0x7f050003;
         public static final int menu_ajout=0x7f050005;
@@ -73,6 +83,7 @@ public final class R {
         public static final int menu_sync=0x7f050006;
         public static final int no_notes=0x7f050004;
         public static final int title=0x7f050009;
+        public static final int title2=0x7f05000a;
     }
     public static final class style {
         /** 
@@ -96,10 +107,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
